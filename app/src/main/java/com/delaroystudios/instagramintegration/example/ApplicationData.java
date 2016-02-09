cc0d1c81247a5daf28b5572989f61e51eae11215
delaroy <bamoyk@yahoo.com>
2016-02-09 18:11:46 +0100

    android instagram
    android instagram

---
+++ app/src/main/java/com/delaroystudios/instagramintegration/example/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.delaroystudios.instagramintegration.example;
+
+public class ApplicationData {
+	public static final String CLIENT_ID = "e79ecfdc005d43ffb415dd2084eef588";
+	public static final String CLIENT_SECRET = "a9f6fcf47e78406089f75e0514b7ec59";
+	public static final String CALLBACK_URL = "http://www.delaroystudios.com/channel";
+}
